d4249c06eb4afcf01c401f051d1281e30cecef28
Sagar Dabgar <sagardabgar2806@gmail.com>
2025-04-26 17:03:05 +0530

    chat with database by sagar dabgar

--- sales_chatbot/chatbot.py
+++ sales_chatbot/chatbot.py
@@ -9,7 +9,7 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
-GOOGLE_GENERATIVE_AI_API_KEY = os.getenv('GEMINI_API_KEY')
+GOOGLE_GENERATIVE_AI_API_KEY = os.getenv('GOOGLE_API_KEY')
 llm = GoogleGenerativeAI(model = "gemini-2.0-flash",api_key=GOOGLE_GENERATIVE_AI_API_KEY,temperature=0.7)
 
 db_connection = init_database()
